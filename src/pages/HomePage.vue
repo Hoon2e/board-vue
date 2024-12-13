550e341195483b88dec71f1ed8cfe82b0c17bb9e
Hoon2e <sanghoon0120@gmail.com>
2024-12-13 17:39:57 +0900

    init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>HomePage</div>
+  <RouterLink to="/login">to login</RouterLink>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>RegisterPage</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
