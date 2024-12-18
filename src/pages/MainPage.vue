bbe9fae07a333189ece6d59a136d91eda158b69d
Hoon2e <sanghoon0120@gmail.com>
2024-12-18 15:06:39 +0900

    feat: Main에서 로그아웃 기능 추가

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,7 +1,21 @@
 <template>
   <div>main</div>
+  <button @click="onClickLogout">logout</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { logoutUser } from '@/api/user'
+import router from '@/router'
+
+const onClickLogout = async () => {
+  try {
+    await logoutUser()
+    alert('logout 성공')
+    router.push('/login')
+  } catch {
+    alert('logout 실패')
+  }
+}
+</script>
 
 <style scoped></style>
